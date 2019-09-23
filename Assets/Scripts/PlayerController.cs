6586575da7057ef084a0001e85cb0c612beb91d5
Andrew Scarlett <anscarlett@rogers.com>
2019-09-23 17:46:48 -0400

    Added bobblehead physics to the Space Marine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed = 50.0f;
+
     private CharacterController characterController;
+    public Rigidbody head;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,19 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if(moveDirection == Vector3.zero)
+        {
+
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 
 } 

212c6cabd385002d404c50b8ba1405526a1c52c4
Andrew Scarlett <anscarlett@rogers.com>
2019-09-26 20:13:26 -0400

    Added animation for SpaceMarine, ending chapter 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
 
+    public Animator bodyAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,11 +32,12 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if(moveDirection == Vector3.zero)
         {
-
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
         }
 
         RaycastHit hit;

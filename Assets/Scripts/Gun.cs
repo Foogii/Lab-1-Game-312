01fce0c544db2e616c2ae57583e738739d5628b5
Andrew Scarlett <anscarlett@rogers.com>
2019-09-29 12:24:16 -0400

    Added sounds for upgraded gun, and the outcome if the gun were to be upgraded (no powerup object yet)

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,6 +9,10 @@ public class Gun : MonoBehaviour
 
     private AudioSource audioSource;
 
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,13 +38,39 @@ public class Gun : MonoBehaviour
 
     void fireBullet()
     {
-        // Create a GameObject called bullet and assign the bulletPrefab to it
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject; 
-        //Set the bullets position to specified location (launchPosition)
-        bullet.transform.position = launchPosition.position;
-        //Retrieve the rigidbody component from the buller and push the object forwards
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
 
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        if(isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;
+
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
     }
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
+    }
+
 }
